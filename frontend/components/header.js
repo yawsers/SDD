e2c70f0e58bb56d8461bb57913f72cf60f98cdad
Annie Shi <annieshi6@gmail.com>
2020-12-08 04:55:07 -0500

    header.js update

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -3,18 +3,16 @@ class Header extends HTMLElement {
 		super();
 	}
 	connectedCallback() {
-		this.innerHTML = `<header><a href="index.html"
-		id="wordmark">Profess.it</a><div id="primary-nav" class="links"><a
-		href="login.html">Sign in</a><a
-		href="register.html">Create an account</a><a
-		href="classes.html">My classes</a><a href="#">Account settings</a><a
-		href="#">Sign out</a></div></header>`;
-		var links = document.getElementById("primary-nav").getElementsByTagName("a");
-		console.log(links);
-		var i;
+		var header = `<header><a href="index.html"
+		id="wordmark">Profess.it</a><div id="primary-nav" class="links">`;
 		if (localStorage.getItem('token'))
-			for (i = 0; i < 2; i++) links[0].remove();
-		else for (i = 0; i < 3; i++) links[2].remove();
+			header += `<a href="classes.html">My classes</a><a
+			href="#">Account settings</a><a href="#">Sign out</a>`;
+		else
+			header += `<a href="login.html">Sign in</a><a
+			href="register.html">Create an account</a>`;
+		header += `</div></header>`;
+		this.innerHTML = header;
 	}
 }
 
