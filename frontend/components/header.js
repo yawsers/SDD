4f0088f0c6ea8bd4e1bc512c231bb62744c45527
Yaon <NYYuHao@gmail.com>
2020-12-08 05:15:18 -0500

    Sign out button now works

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -7,13 +7,21 @@ class Header extends HTMLElement {
 		id="wordmark">Profess.it</a><div id="primary-nav" class="links">`;
 		if (localStorage.getItem('token'))
 			header += `<a href="classes.html">My classes</a><a
-			href="#">Account settings</a><a href="#">Sign out</a>`;
+			href="#">Account settings</a><a href="#"
+			onmouseup="signOut()">Sign out</a>`;
 		else
 			header += `<a href="login.html">Sign in</a><a
 			href="register.html">Create an account</a>`;
 		header += `</div></header>`;
 		this.innerHTML = header;
 	}
+}
+
+// Logs the current user out by clearing localStorage()
+// and redirecting to index page
+function signOut() {
+	localStorage.clear();
+	window.location.replace('../pages/index.html')
 }
 
 customElements.define('header-component', Header);
