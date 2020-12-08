79d2e87fe572b9e850147afcec4963fbb4273772
Yaon <NYYuHao@gmail.com>
2020-12-08 03:14:01 -0500

    Classes now load depending on user

--- frontend/scripts/onload.js
+++ frontend/scripts/onload.js
@@ -17,22 +17,38 @@ function loadLogin() {
 // If the user is logged in, load the users respective classes
 // If not logged in, redirect to login page
 function loadClasses() {
+	if (!isLoggedIn()) {
+        	window.location.replace('login.html');
+	}
+
 	const http = new XMLHttpRequest();
 
-	// Student load
-	http.open("POST", "https://professit-backend.herokuapp.com/get_all_classes")
-	http.setRequestHeader("Content-Type", "application/json");
-	http.send(JSON.stringify({}));
-
+	// Load student classes
+	if (localStorage.isstudent == "true") {
+		http.open("POST", "https://professit-backend.herokuapp.com/get_all_classes")
+		http.setRequestHeader("Content-Type", "application/json");
+		http.send(JSON.stringify({"studentid": localStorage.token}));
+		document.getElementById('create-class').style.display = "none"
+	}
+	// Load instructor classes
+	else {
+		http.open("POST", "https://professit-backend.herokuapp.com/get_all_teaching")
+		http.setRequestHeader("Content-Type", "application/json");
+		http.send(JSON.stringify({"instructorid": localStorage.token}));
+		document.getElementById('join-class').style.display = "none"
+	}
+	// Fill page with classes
 	http.onload = function() {
         	const response = JSON.parse(http.responseText);
-		// Login successful, redirect to classes page
-		if (response.status) {
-                	localStorage.setItem('token', response.usid)
-			window.location.replace('classes.html');
-		}
-		else {
-                	alert("Invalid login");
+		console.log(response);
+		const classdiv = document.getElementById("classes");
+		for (var i = 0; i < response.result.length; i++) {
+			classdiv.innerHTML += "<a href=class.html?id=" +
+				response.result[i].classid + "> <div>" +
+				response.result[i].classname + "</div>" +
+				(response.result[i].instructor ? "<div>" +
+				response.result[i].instructor + "</div>" : "") +
+				"</a>"
 		}
 	}
 }
