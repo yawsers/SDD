f55a6f2b96139d4713db9c91e20f396d95d75ab9
Yaon <NYYuHao@gmail.com>
2020-12-14 01:37:49 -0500

    Students can be added from studentlist (doesn't load students yet)

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -10,12 +10,15 @@ class Nav extends HTMLElement {
 			if (localStorage.getItem('isstudent') == "true")
 				nav += `<a href="#">Join a class</a>`;
 			else nav += `<a href="#">Create a class</a>`;
-		} else if (loc == "class") {
+		} else if (loc == "class" || loc == "studentlist") {
+			const urlParams = new URLSearchParams(window.location.search);
+			const classid = urlParams.get('id');
 			// update these w specific urls based on class
-			nav += `<a href="class.html">Lectures</a><a href="#">Resources</a>`;
+			nav += `<a href="class.html?id=` + classid + `">Lectures</a><a href="#">Resources</a>`;
 			if (localStorage.getItem('isstudent') == "true")
 				nav += `<a href="#" onmouseup="redirectLecture()">Join the lecture</a><a href="#">Leave this class</a>`;
 			else {
+				nav += `<a href="studentlist.html?id=` + classid + `">Student List</a>`;
 				nav += `<a href="#" onmouseup="redirectLecture()">Start a lecture</a><a href="#">Class settings</a>`;
 			}
 		}
@@ -27,5 +30,5 @@ class Nav extends HTMLElement {
 customElements.define('secondary-nav', Nav);
 
 function redirectLecture() {
-	window.location.replace("localhost:8000");
+	window.open("localhost:8000/video_feed");
 }
